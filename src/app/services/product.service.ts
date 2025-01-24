41e7553e6eb53a7ab05d05f069e24ecf410d8c52
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-01-24 19:09:13 -0300

    feat: enhance product service with reactive state management for product list

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { inject, Injectable, signal } from '@angular/core';
+import { Observable, tap } from 'rxjs';
 import { IProduct, IResponseProduct } from '../interfaces/product.interface';
 
 @Injectable({
@@ -10,9 +10,12 @@ export class ProductService {
   http = inject(HttpClient);
 
   private apiUrl = 'http://rest-items.research.cloudonix.io/items';
+  products = signal<IProduct[]>([]);
 
   getProducts(): Observable<IProduct[]> {
-    return this.http.get<IProduct[]>(this.apiUrl);
+    return this.http
+      .get<IProduct[]>(this.apiUrl)
+      .pipe(tap((products) => this.products.set(products)));
   }
 
   getProductById(id: string): Observable<IProduct> {
@@ -20,17 +23,61 @@ export class ProductService {
   }
 
   addProduct(product: IResponseProduct): Observable<IResponseProduct> {
-    return this.http.post<IResponseProduct>(this.apiUrl, product);
+    return this.http
+      .post<IResponseProduct>(this.apiUrl, product)
+      .pipe(
+        tap((newProduct) =>
+          this.products.update((products) => [...products, newProduct])
+        )
+      );
   }
 
   updateProduct(product: IResponseProduct): Observable<IResponseProduct> {
-    return this.http.patch<IResponseProduct>(
-      `${this.apiUrl}/${product.id}`,
-      product
-    );
+    return this.http
+      .patch<IResponseProduct>(`${this.apiUrl}/${product.id}`, product)
+      .pipe(
+        tap((updatedProduct) =>
+          this.products.update((products) =>
+            products.map((p) =>
+              p.id === updatedProduct.id ? updatedProduct : p
+            )
+          )
+        )
+      );
   }
 
   deleteProduct(id: number): Observable<IProduct> {
-    return this.http.delete<IProduct>(`${this.apiUrl}/${id}`);
+    return this.http
+      .delete<IProduct>(`${this.apiUrl}/${id}`)
+      .pipe(
+        tap(() =>
+          this.products.update((products) =>
+            products.filter((product) => product.id !== id)
+          )
+        )
+      );
   }
+
+  // getProducts(): Observable<IProduct[]> {
+  //   return this.http.get<IProduct[]>(this.apiUrl);
+  // }
+
+  // getProductById(id: string): Observable<IProduct> {
+  //   return this.http.get<IProduct>(`${this.apiUrl}/${id}`);
+  // }
+
+  // addProduct(product: IResponseProduct): Observable<IResponseProduct> {
+  //   return this.http.post<IResponseProduct>(this.apiUrl, product);
+  // }
+
+  // updateProduct(product: IResponseProduct): Observable<IResponseProduct> {
+  //   return this.http.patch<IResponseProduct>(
+  //     `${this.apiUrl}/${product.id}`,
+  //     product
+  //   );
+  // }
+
+  // deleteProduct(id: number): Observable<IProduct> {
+  //   return this.http.delete<IProduct>(`${this.apiUrl}/${id}`);
+  // }
 }
