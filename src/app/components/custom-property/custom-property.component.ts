1abe8723d760b50f592ad9de7e96809948eef1ae
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-01-24 20:08:08 -0300

    feat: add custom property component with dynamic form handling

---
+++ src/app/components/custom-property/custom-property.component.ts
@@ -0,0 +1,104 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject, Input } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-custom-property',
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+  templateUrl: './custom-property.component.html',
+  styleUrl: './custom-property.component.scss',
+})
+export class CustomPropertyComponent {
+  formBuilder = inject(FormBuilder);
+
+  @Input() parentForm!: FormGroup;
+
+  // Garantimos que `customProperties` é um FormArray.
+  get customProperties(): FormArray {
+    const customPropertiesControl = this.parentForm.get('customProperties');
+    if (customPropertiesControl instanceof FormArray) {
+      return customPropertiesControl;
+    }
+    throw new Error('customProperties is not a FormArray');
+  }
+
+  addCustomProperty() {
+    const customProp = this.formBuilder.group({
+      key: ['', Validators.required],
+      value: ['', Validators.required],
+    });
+    this.customProperties.push(customProp);
+  }
+
+  removeCustomProperty(index: number) {
+    this.customProperties.removeAt(index);
+  }
+
+  // @Input() parentForm!: FormGroup;
+
+  // get customProperties(): FormArray {
+  //   return (
+  //     (this.parentForm?.get('customProperties') as FormArray) ||
+  //     this.formBuilder.array([])
+  //   );
+  // }
+
+  // addCustomProperty() {
+  //   const customProp = this.formBuilder.group({
+  //     key: ['', Validators.required],
+  //     value: ['', Validators.required],
+  //   });
+  //   this.customProperties.push(customProp);
+  // }
+
+  // removeCustomProperty(index: number) {
+  //   this.customProperties.removeAt(index);
+  // }
+
+  // @Input() parentForm!: FormGroup;
+
+  // get customProperties(): FormArray {
+  //   return this.parentForm.get('customProperties') as FormArray;
+  // }
+
+  // addCustomProperty() {
+  //   const customProp = this.formBuilder.group({
+  //     key: ['', Validators.required],
+  //     value: ['', Validators.required],
+  //   });
+  //   this.customProperties.push(customProp);
+  // }
+
+  // removeCustomProperty(index: number) {
+  //   this.customProperties.removeAt(index);
+  // }
+
+  // @Input() customProperties!: FormArray;
+
+  // addCustomProperty() {
+  //   const customProp = this.formBuilder.group({
+  //     key: ['', Validators.required],
+  //     value: ['', Validators.required],
+  //   });
+  //   this.customProperties.push(customProp);
+  // }
+
+  // removeCustomProperty(index: number) {
+  //   this.customProperties.removeAt(index);
+  // }
+}
