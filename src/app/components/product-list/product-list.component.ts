ebc64561362ea0be5e28bfe0c509ace05a019a13
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-01-24 14:00:35 -0300

    feat: add delete functionality to ProductListComponent with confirmation dialog

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
 import { MatTableModule } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { IProduct } from '../../interfaces/product.interface';
 import { ProductService } from '../../services/product.service';
 import { AddProductComponent } from '../add-product/add-product.component';
+import { DeleteDialogComponent } from '../delete-dialog/delete-dialog.component';
 
 @Component({
   selector: 'app-product-list',
@@ -15,6 +17,7 @@ import { AddProductComponent } from '../add-product/add-product.component';
 export class ProductListComponent implements OnInit {
   productService = inject(ProductService);
   router = inject(Router);
+  dialog = inject(MatDialog);
   products: IProduct[] = [];
   showModal: boolean = false;
   displayedColumns: string[] = ['index', 'sku', 'name', 'price', 'actions'];
@@ -32,5 +35,27 @@ export class ProductListComponent implements OnInit {
 
   onView(product: IProduct) {
     this.router.navigate(['/product', product.id]);
+  }
+
+  onDelete(id: number) {
+    const dialogRef = this.dialog.open(DeleteDialogComponent, {
+      width: '250px',
+      data: { id },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.productService.deleteProduct(id).subscribe({
+          next: () => {
+            this.products = this.products.filter(
+              (product) => product.id !== id
+            );
+          },
+          error: (error) => {
+            console.error('Error deleting product:', error);
+          },
+        });
+      }
+    });
   }
 }
