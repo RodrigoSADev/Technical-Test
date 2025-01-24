b8e2a9ac386d9315aa961b7ac6ba450b236bda22
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-01-24 12:18:31 -0300

    feat: add route for lazy loading ProductListComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,4 +4,11 @@ import { LoginComponent } from './components/login/login.component';
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('./components/product-list/product-list.component').then(
+        (m) => m.ProductListComponent
+      ),
+  },
 ];
