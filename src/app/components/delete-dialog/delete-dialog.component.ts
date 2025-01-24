f7ec81e97ddd03c48f10051efbef42830df70df7
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-01-24 13:59:49 -0300

    feat: add DeleteDialogComponent for item deletion confirmation

---
+++ src/app/components/delete-dialog/delete-dialog.component.ts
@@ -0,0 +1,33 @@
+import { Component, Inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-delete-dialog',
+  imports: [
+    MatDialogClose,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogTitle,
+    MatButtonModule,
+  ],
+  templateUrl: './delete-dialog.component.html',
+  styleUrl: './delete-dialog.component.scss',
+})
+export class DeleteDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<DeleteDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { id: number }
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
