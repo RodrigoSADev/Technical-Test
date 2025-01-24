eccab4e18113d9cfb87f63c58ea1abdb79350723
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-01-24 12:05:37 -0300

    feat: implement authInterceptor for handling authentication tokens in HTTP requests

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,40 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+// export const httpAuthErrorsInterceptor: HttpInterceptorFn = (req, next) => {
+//   const authService = inject(AuthService);
+//   const authToken = authService.getAuthKey();
+
+//   const clonedRequest = req.clone({
+//     setHeaders: {
+//       Authorization: `Bearer ${authToken}`,
+//     },
+//   });
+
+//   return next(clonedRequest).pipe(
+//     catchError((error) => {
+//       console.log('Deu erro');
+//       return throwError(() => error);
+//     })
+//   );
+// };
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+  const authToken = authService.getAuthKey();
+
+  const clonedRequest = req.clone({
+    setHeaders: {
+      Authorization: `Bearer ${authToken}`,
+    },
+  });
+
+  return next(clonedRequest).pipe(
+    catchError((error) => {
+      console.log('Deu erro');
+      return throwError(() => error);
+    })
+  );
+};
